f8b088ef757ca7c5fbaeddb30f82d5b039cebf03
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-05-30 21:27:56 +0530

    Updates

---
+++ precache-manifest.7a694b7a5a13c96cd48f44632a3a77e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "780955baf06dbd94cc9436e924d65613",
+    "url": "/linux_permissions/index.html"
+  },
+  {
+    "revision": "e3cbe14f131e77a0733d",
+    "url": "/linux_permissions/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2b1b2e77caa8ca42001b",
+    "url": "/linux_permissions/static/js/2.5e8dfd01.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/linux_permissions/static/js/2.5e8dfd01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3cbe14f131e77a0733d",
+    "url": "/linux_permissions/static/js/main.fc373736.chunk.js"
+  },
+  {
+    "revision": "354b5ef020252e127873",
+    "url": "/linux_permissions/static/js/runtime-main.578c7f0f.js"
+  }
+]);
